f3f52d96fe77d07d18e8f4bcae28c64588bc112f
Carlos Aguilera <ckarlos_95@hotmail.es>
2023-09-23 16:28:25 -0600

    Add match components

---
+++ src/components/main/match/MatchCard.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+import { type Dog } from '~/types'
+import { ageDescription } from '~/utils/dogInfo'
+
+const { dog } = defineProps<{
+  dog: Dog
+}>()
+
+const { age, breed, img: image, name, location } = toRefs(dog)
+</script>
+
+<template>
+  <div
+    class="w-full grid grid-cols-1 gap-y-8 gap-x-6 items-start sm:grid-cols-12 lg:gap-x-8 mt-8"
+    sm="flex items-center"
+  >
+    <div class="sm:col-span-6 lg:col-span-7 flex-1">
+      <div
+        class="aspect-ratio-square rounded-xl bg-gray-100 overflow-hidden lg:rounded-3xl"
+      >
+        <img
+          :src="image"
+          :alt="name"
+          class="object-center object-cover min-w-full min-h-full lg:min-h-md lg:min-w-md"
+        />
+      </div>
+    </div>
+    <div class="sm:col-span-6 lg:col-span-5 flex-1 flex-grow">
+      <div class="bg-white shadow overflow-hidden sm:rounded-lg">
+        <div class="px-4 py-5 sm:px-6">
+          <h3 class="text-lg leading-6 font-medium text-gray-900">
+            Here are the details for your ideal pup!
+          </h3>
+        </div>
+        <div class="border-t border-gray-200">
+          <dl>
+            <div
+              class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
+            >
+              <dt class="text-sm font-medium text-gray-500">Name</dt>
+              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                {{ name }}
+              </dd>
+            </div>
+            <div
+              class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
+            >
+              <dt class="text-sm font-medium text-gray-500">Breed</dt>
+              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                {{ breed }}
+              </dd>
+            </div>
+            <div
+              class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
+            >
+              <dt class="text-sm font-medium text-gray-500">Age</dt>
+              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                {{ ageDescription(age) }}
+              </dd>
+            </div>
+            <div
+              class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
+            >
+              <dt class="text-sm font-medium text-gray-500">Lives in</dt>
+              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                <p>{{ location.state }},</p>
+                <p>{{ location.city }},</p>
+                <p>{{ location.county }}</p>
+                <p>Zip Code: {{ location.zip_code }}</p>
+              </dd>
+            </div>
+          </dl>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/main/match/MatchModal.vue
@@ -0,0 +1,101 @@
+<script setup lang="ts">
+import { type Dog as DogType } from '~/types'
+
+const { matchedDog } = defineProps<{
+  matchedDog?: DogType
+}>()
+
+const isOpen = defineModel<boolean>('isOpen', { required: true })
+</script>
+
+<template>
+  <TransitionRoot as="template" :show="isOpen">
+    <Dialog
+      as="div"
+      class="fixed z-10 inset-0 overflow-y-auto"
+      @close="isOpen = false"
+    >
+      <div
+        class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
+      >
+        <TransitionChild
+          as="template"
+          enter="ease-out duration-300"
+          enter-from="opacity-0"
+          enter-to="opacity-100"
+          leave="ease-in duration-200"
+          leave-from="opacity-100"
+          leave-to="opacity-0"
+        >
+          <DialogOverlay
+            class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+          />
+        </TransitionChild>
+
+        <!-- This element is to trick the browser into centering the modal contents. -->
+        <span
+          class="hidden sm:inline-block sm:align-middle sm:h-screen"
+          aria-hidden="true"
+          >&#8203;</span
+        >
+        <TransitionChild
+          as="template"
+          enter="ease-out duration-300"
+          enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          enter-to="opacity-100 translate-y-0 sm:scale-100"
+          leave="ease-in duration-200"
+          leave-from="opacity-100 translate-y-0 sm:scale-100"
+          leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+        >
+          <div
+            class="relative inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-xl w-full sm:p-6 lg:max-w-4xl"
+          >
+            <div class="hidden sm:block absolute top-0 right-0 pt-4 pr-4">
+              <button
+                type="button"
+                class="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                @click="isOpen = false"
+              >
+                <span class="sr-only">Close</span>
+                <div
+                  class="i-heroicons:x-mark-20-solid h-6 w-6"
+                  aria-hidden="true"
+                />
+              </button>
+            </div>
+            <div class="sm:flex sm:items-start">
+              <div
+                class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-indigo-100 sm:mx-0 sm:h-10 sm:w-10"
+              >
+                <ConfettiIcon
+                  class="h-6 w-6 text-indigo-600"
+                  aria-hidden="true"
+                />
+              </div>
+              <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                <DialogTitle
+                  as="h3"
+                  class="text-3xl leading-6 font-medium text-gray-900"
+                >
+                  It's a match!
+                </DialogTitle>
+                <div class="mt-2">
+                  <MatchCard v-if="matchedDog" :dog="matchedDog" />
+                </div>
+              </div>
+            </div>
+            <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+              <button
+                type="button"
+                class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+                @click="isOpen = false"
+              >
+                OK
+              </button>
+            </div>
+          </div>
+        </TransitionChild>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+</template>
